c4d3c024bce792834289e7f5f52fa28378cda572
Tyson London <tyson@modcul.com>
2023-01-09 22:17:59 +0100

    removed hashing

--- backend/app/Console/Commands/PopulateProductionUsers.php
+++ backend/app/Console/Commands/PopulateProductionUsers.php
@@ -35,9 +35,8 @@ class PopulateProductionUsers extends Command
         $user = User::create([
             'name' => ' Agency Analytics',
             'email' => 'info@agencyanalytics.com',
-            'password' => Hash::make('$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'email_verified_at' => now(),
-            'remember_token' => Str::random(10),
         ]);
 
         return Command::SUCCESS;
